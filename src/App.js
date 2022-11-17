897a8792eadd2fc4ee71804de0a7136b27c21c72
RajuRk <45646335+RajuRk@users.noreply.github.com>
2022-11-17 16:26:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Header from './components/Header'
-import Offer from './components/OurOffering'
+import Header from './components/Header';
+import Offer from './components/OurOffering';
+import Portfolio from './components/Portfolio';
+import Testimonial from './components/Testimonials';
+import Project from './components/Project';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header/>
       <Offer/>
+      <Portfolio/>
+      <Testimonial/>
+      <Project/>
+      <Footer/>
     </>
   );
 }
